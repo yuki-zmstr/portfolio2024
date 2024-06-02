0b3a589dc3ce142fd00dfccae047d4184d249c67
yuki-zmstr <yukihide99@gmail.com>
2024-06-02 16:09:19 +0800

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+        <>
+            <p>
+                This project is built using React with <a href="https://vitejs.dev/" target="_blank">Vite</a>.
+                I started out using&nbsp;
+                <a href="https://astro.build/" target="_blank">Astro</a>, a JavaScript-based static site generator. Astro projects are component-based simliar to React, though by default components are rendered on the server, and the user will be delivered content in HTML.
+
+                On the flip side it doesn't offer client-side interaction, such as access to the <code>DOM</code> or <code>window</code>. It seems that Astro (as described in their website) is designed for content-heavy websites with multiple pages to display, such as blogs, and may not have been the best choice for this project.
+
+                To achieve the SPA experience, I ultimately chose React. At the least I got to understand what Astro is trying to achieve compared to SPA frameworks. The source code for this project can be found&nbsp;
+                <a href="https://github.com/yuki-zmstr/portfolio2024" target="_blank">here</a>.
+            </p>
+            <p>Previous Websites</p>
+            <p>&copy; Yukihide Takahashi 2024</p>
+        </>)
+}
+
