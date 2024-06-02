4882b1b129202db46045ed3e52c2ddfb4c3ba9d4
yuki-zmstr <yukihide99@gmail.com>
2024-06-02 16:09:27 +0800

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+import "../styles/header.css";
+
+export default function Header() {
+    return (
+        <>
+            <h2>Yukihide</h2>
+            <div className="intro">
+                <p>
+                    Hi, my name is Yukihide, and I'm a software developer working in Tokyo, Japan. Welcome to my portfolio page.
+                </p>
+
+                <p>
+                    I've chosen a simple design for this website because of the amount of
+                    written content I wanted to include. It looks more plain than my
+                    previous websites, which can be found in the footnote.
+                </p>
+            </div>
+        </>)
+}
+
