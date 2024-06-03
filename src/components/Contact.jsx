c3464e84a7a786428996a8e17a20912416fdfb06
yuki-zmstr <yukihide99@gmail.com>
2024-06-03 14:27:29 +0800

    Add link to CV

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,9 @@
 export default function Contact() {
     return (
         <div>
-            <p>CV: link to CV</p>
+            <p>CV:&nbsp;
+                <a href="https://drive.google.com/file/d/1Qi5r1uxP5vdCFeXW-jC5V7GCIV7AslCk/view" target="_blank">Google Drive</a>
+            </p>
             <p>Contact: yukihide99[at]gmail.com</p>
         </div>
     )
