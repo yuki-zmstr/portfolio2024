fd7b6a6ab177359155d3c907e08c00e6b6a23a95
yuki-zmstr <yukihide99@gmail.com>
2025-05-17 09:24:41 +0900

    Update CV link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,10 @@ export default function Contact() {
     return (
         <div>
             <p>CV:&nbsp;
-                <a href="https://drive.google.com/file/d/1Qi5r1uxP5vdCFeXW-jC5V7GCIV7AslCk/view" target="_blank">Google Drive</a>
+                <a href="https://drive.google.com/file/d/1BfER0n_5UdtAroDfYz1o_0Q5O3N8AEak/view" target="_blank">Google Drive</a>
             </p>
             <p>Contact: yukihide99[at]gmail.com</p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/yukihide-takahashi/" target="_blank">link</a></p>
         </div>
     )
 }
